758c54340af5e4ce1708376cd6903b52430d4844
Duyen Nguyen <hongduyen0705@gmail.com>
2022-01-20 18:34:27 +0200

    scroll to top button

--- script.js
+++ script.js
@@ -18,13 +18,36 @@ const navSlide = () => {
         // burger Animation 
         burger.classList.toggle('toggle');
     })
-
-
 }
-
 navSlide();
 
-// Text Animation 
+// Hide or show the scroll button
+const scrollBtn = document.querySelector('.scroll-to-top');
+
+const scrollButtonVisibility = () => {
+    if (document.documentElement.scrollTop <= 150) {
+        scrollBtn.style.display = 'none';
+    } else {
+        scrollBtn.style.display = 'block';
+    }
+}
+
+scrollButtonVisibility();
+
+// Scroll to top
+scrollBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
+
+// Hide or show the scroll button 
+document.addEventListener('scroll', (e) => {
+    scrollButtonVisibility();
+})
+// Text Animation
 // const text = document.querySelector('.duyen');
 // const strText = text.textContent;
 // const splitText = strText.split("");
